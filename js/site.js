5b5ea7033e7da4d68e36aa95c2cd1def919d8ea2
NAME <EMAIL>
2021-09-10 17:07:03 +0800

    with the website also

--- js/site.js
+++ js/site.js
@@ -1,19 +1,41 @@
-const uri = 'https://jsonplaceholder.typicode.com/users';
+let todos = [];
+
+function getItems() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => _displayItems(data))
+      .catch(error => console.error('Unable to get items.', error));
+}
+
+function _displayItems(data) {
+    const tBody = document.getElementById('todos');
+    tBody.innerHTML = '';
+
+    data.forEach(item => {
+        let tr = tBody.insertRow();
+
+        let td1 = tr.insertCell(0);
+        let textNode1 = document.createTextNode(item.id);
+        td1.appendChild(textNode1);
+
+        let td2 = tr.insertCell(1);
+        let textNode2 = document.createTextNode(item.name);
+        td2.appendChild(textNode2);
+
+        let td3 = tr.insertCell(2);
+        let textNode3 = document.createTextNode(item.username);
+        td3.appendChild(textNode3);
+
+        let td4= tr.insertCell(3);
+        let textNode4 = document.createTextNode(item.email);
+        td4.appendChild(textNode4);
+
+        let td5= tr.insertCell(4);
+        let textNode5 = document.createTextNode(item.website);
+        td5.appendChild(textNode5);
 
 
-  /* need to use onload function */
-var formData = new FormData();
-var fileField = document.querySelector("input[type='file']");
-  
-  formData.append('#id', fileField.files[0]);
-  formData.append('name', fileField.files[1]);
-  formData.append('username', fileField.files[2]);
-  formData.append('email', fileField.files[3]);
+    });
 
-  fetch('https://jsonplaceholder.typicode.com/users', {
-    method: 'PUT',
-    body: formData
-  })
-  .then(response => response.json())
-  .catch(error => console.error('Error:', error))
-  .then(response => console.log('Success:', JSON.stringify(response)))
+    todos = data;
+}
