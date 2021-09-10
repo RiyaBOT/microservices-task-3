1071bca03017a061c9303a148b0364fe2f712421
NAME <EMAIL>
2021-09-10 16:30:39 +0800

    Trial1

---
+++ js/site.js
@@ -0,0 +1,18 @@
+const uri = 'https://jsonplaceholder.typicode.com/users';
+const fetch = require("node-fetch");
+const data = { username: 'example' };
+
+fetch('https://example.com/profile', {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
