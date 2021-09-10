2b893a1d3303fb985e34e6676257bff42a06dfae
NAME <EMAIL>
2021-09-10 16:34:30 +0800

    trial2

--- js/site.js
+++ js/site.js
@@ -1,18 +1,7 @@
 const uri = 'https://jsonplaceholder.typicode.com/users';
-const fetch = require("node-fetch");
-const data = { username: 'example' };
+arr=[]
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => console.log(data))
 
-fetch('https://example.com/profile', {
-  method: 'POST', // or 'PUT'
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
-})
-.then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
+arr= data
